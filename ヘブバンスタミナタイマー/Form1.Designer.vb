cccb3419bad498b17eff51c0c4548b93206e8d22
大狼ユタ <yuta.vtuber@gmail.com>
2025-01-19 12:49:56 +0900

    プロジェクト ファイルを追加します。

---
+++ ヘブバンスタミナタイマー/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        lblTimer = New Label()
+        btnSet = New Button()
+        btnStart = New Button()
+        btnStop = New Button()
+        timerCountdown = New Timer(components)
+        txtHours = New TextBox()
+        txtMinutes = New TextBox()
+        txtSeconds = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Life = New NumericUpDown()
+        Label3 = New Label()
+        errorcorrection = New Button()
+        CheckBox1 = New CheckBox()
+        AddDate = New Label()
+        CType(Life, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' lblTimer
+        ' 
+        lblTimer.AutoSize = True
+        lblTimer.Font = New Font("Yu Gothic UI", 18F)
+        lblTimer.Location = New Point(7, 7)
+        lblTimer.Margin = New Padding(4, 0, 4, 0)
+        lblTimer.Name = "lblTimer"
+        lblTimer.Size = New Size(102, 32)
+        lblTimer.TabIndex = 0
+        lblTimer.Text = "00:00:00"
+        ' 
+        ' btnSet
+        ' 
+        btnSet.Location = New Point(116, 123)
+        btnSet.Margin = New Padding(4, 3, 4, 3)
+        btnSet.Name = "btnSet"
+        btnSet.Size = New Size(54, 23)
+        btnSet.TabIndex = 1
+        btnSet.Text = "設定"
+        btnSet.UseVisualStyleBackColor = True
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(62, 181)
+        btnStart.Margin = New Padding(4, 3, 4, 3)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(75, 22)
+        btnStart.TabIndex = 2
+        btnStart.Text = "開始"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' btnStop
+        ' 
+        btnStop.Location = New Point(143, 181)
+        btnStop.Margin = New Padding(4, 3, 4, 3)
+        btnStop.Name = "btnStop"
+        btnStop.Size = New Size(75, 22)
+        btnStop.TabIndex = 3
+        btnStop.Text = "停止"
+        btnStop.UseVisualStyleBackColor = True
+        ' 
+        ' timerCountdown
+        ' 
+        ' 
+        ' txtHours
+        ' 
+        txtHours.Location = New Point(8, 123)
+        txtHours.Margin = New Padding(4, 3, 4, 3)
+        txtHours.MaximumSize = New Size(19, 23)
+        txtHours.Name = "txtHours"
+        txtHours.Size = New Size(19, 23)
+        txtHours.TabIndex = 4
+        txtHours.Text = "00"
+        ' 
+        ' txtMinutes
+        ' 
+        txtMinutes.Location = New Point(48, 123)
+        txtMinutes.Margin = New Padding(4, 3, 4, 3)
+        txtMinutes.MaximumSize = New Size(19, 23)
+        txtMinutes.Name = "txtMinutes"
+        txtMinutes.Size = New Size(19, 23)
+        txtMinutes.TabIndex = 5
+        txtMinutes.Text = "00"
+        ' 
+        ' txtSeconds
+        ' 
+        txtSeconds.Location = New Point(89, 123)
+        txtSeconds.Margin = New Padding(4, 3, 4, 3)
+        txtSeconds.MaximumSize = New Size(19, 23)
+        txtSeconds.Name = "txtSeconds"
+        txtSeconds.Size = New Size(19, 23)
+        txtSeconds.TabIndex = 6
+        txtSeconds.Text = "00"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(32, 127)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(10, 15)
+        Label1.TabIndex = 7
+        Label1.Text = ":"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(73, 127)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(10, 15)
+        Label2.TabIndex = 8
+        Label2.Text = ":"
+        ' 
+        ' Life
+        ' 
+        Life.AccessibleRole = AccessibleRole.None
+        Life.Font = New Font("Yu Gothic UI", 18F)
+        Life.ImeMode = ImeMode.Off
+        Life.Location = New Point(222, 4)
+        Life.Margin = New Padding(4, 3, 4, 3)
+        Life.Maximum = New Decimal(New Integer() {5, 0, 0, 0})
+        Life.Name = "Life"
+        Life.ReadOnly = True
+        Life.Size = New Size(38, 39)
+        Life.TabIndex = 9
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Yu Gothic UI", 15F)
+        Label3.Location = New Point(112, 9)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(102, 28)
+        Label3.TabIndex = 10
+        Label3.Text = "現在のLIFE"
+        ' 
+        ' errorcorrection
+        ' 
+        errorcorrection.Location = New Point(178, 123)
+        errorcorrection.Margin = New Padding(4, 3, 4, 3)
+        errorcorrection.Name = "errorcorrection"
+        errorcorrection.Size = New Size(90, 23)
+        errorcorrection.TabIndex = 11
+        errorcorrection.Text = "誤差修正-1秒"
+        errorcorrection.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(157, 156)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(107, 19)
+        CheckBox1.TabIndex = 12
+        CheckBox1.Text = "常に最前面表示"
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' AddDate
+        ' 
+        AddDate.AutoSize = True
+        AddDate.Font = New Font("Yu Gothic UI", 11F)
+        AddDate.Location = New Point(12, 40)
+        AddDate.Name = "AddDate"
+        AddDate.Size = New Size(53, 20)
+        AddDate.TabIndex = 13
+        AddDate.Text = "Label4"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        AutoSize = True
+        ClientSize = New Size(273, 211)
+        Controls.Add(AddDate)
+        Controls.Add(CheckBox1)
+        Controls.Add(errorcorrection)
+        Controls.Add(Label3)
+        Controls.Add(Life)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtSeconds)
+        Controls.Add(txtMinutes)
+        Controls.Add(txtHours)
+        Controls.Add(btnStop)
+        Controls.Add(btnStart)
+        Controls.Add(btnSet)
+        Controls.Add(lblTimer)
+        Margin = New Padding(4, 3, 4, 3)
+        MaximumSize = New Size(289, 250)
+        MinimumSize = New Size(289, 250)
+        Name = "Form1"
+        Text = "ヘブバンライフタイマー"
+        TopMost = True
+        CType(Life, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTimer As Label
+    Friend WithEvents btnSet As Button
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents timerCountdown As Timer
+    Friend WithEvents txtHours As TextBox
+    Friend WithEvents txtMinutes As TextBox
+    Friend WithEvents txtSeconds As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Life As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents errorcorrection As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents AddDate As Label
+
+End Class
---
+++ ヘブバンスタミナタイマー/Form1.vb
@@ -0,0 +1,194 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.IO
+Imports System.Speech.Synthesis ' テキスト読み上げ用
+
+
+Public Class Form1
+    ' 残り時間を保持する変数（秒単位）
+    Private remainingTime As Integer
+    ' 音声合成のためのSpeechSynthesizer
+    Private ReadOnly synthesizer As New SpeechSynthesizer()
+    ' 保存ファイルのパス
+    Private Const SaveFilePath As String = "timer_data.csv"
+    ' フォームロード時の初期設定
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTimer.Text = "00:00:00"
+        LoadTimerData()
+    End Sub
+
+    ' フォームが閉じる際にデータを保存
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        SaveTimerData()
+    End Sub
+
+    ' データを保存
+    Private Sub SaveTimerData()
+        Try
+            Dim currentTime As DateTime = DateTime.Now
+            Using writer As New StreamWriter(SaveFilePath)
+                writer.WriteLine($"{remainingTime},{currentTime:O},{Life.Value}") ' Life.Valueも保存
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("タイマーのデータ保存中にエラーが発生しました。", "エラー")
+        End Try
+    End Sub
+
+    ' データを読み込み
+    Private Sub LoadTimerData()
+        If File.Exists(SaveFilePath) Then
+            Try
+                Using reader As New StreamReader(SaveFilePath)
+                    Dim line As String = reader.ReadLine()
+                    If Not String.IsNullOrEmpty(line) Then
+                        Dim parts As String() = line.Split(","c)
+                        Dim savedRemainingTime As Integer = Integer.Parse(parts(0))
+                        Dim savedTimestamp As DateTime = DateTime.Parse(parts(1))
+                        Dim savedLifeValue As Integer = Integer.Parse(parts(2))
+
+                        ' 経過時間を計算
+                        Dim elapsedTime As TimeSpan = DateTime.Now - savedTimestamp
+                        Dim elapsedSeconds As Integer = CInt(elapsedTime.TotalSeconds)
+
+                        ' 閉じていた間の進行をシミュレーション
+                        Dim adjustedTime = savedRemainingTime - elapsedSeconds
+
+                        ' スタミナ更新と時間調整をシミュレート
+                        While adjustedTime <= 0
+                            If savedLifeValue < 5 Then
+                                ' 4時間を追加し、スタミナを1増加
+                                adjustedTime += 4 * 3600
+                                savedLifeValue += 1
+                            Else
+                                ' スタミナがマックスなら停止
+                                adjustedTime = 0
+                                timerCountdown.Stop()
+                                Exit While
+                            End If
+                        End While
+
+                        ' 調整後の値を反映
+                        remainingTime = adjustedTime
+                        Life.Value = savedLifeValue
+                        UpdateTimerLabel()
+                        timerCountdown.Interval = 1000 ' 1秒間隔
+                        timerCountdown.Start()
+                        SpeakNotification（$"前回の情報からタイマーを再開しています。現在のライフは{Life.Value }個です。誤差がある場合は修正をお願いします。"）
+                    End If
+                End Using
+            Catch ex As Exception
+                SpeakNotification("タイマーのデータ読み込み中にエラーが発生しました。")
+            End Try
+        End If
+    End Sub
+
+    ' 設定ボタン
+    Private Sub btnSet_Click(sender As Object, e As EventArgs) Handles btnSet.Click
+        Dim hours As Integer
+        Dim minutes As Integer
+        Dim seconds As Integer
+
+        ' 入力値を取得し、エラーチェックを行う
+        If Not Integer.TryParse(txtHours.Text, hours) OrElse hours < 0 Then
+            SpeakNotification("正しい時を入力してください。")
+            Return
+        End If
+        If Not Integer.TryParse(txtMinutes.Text, minutes) OrElse minutes < 0 OrElse minutes >= 60 Then
+            SpeakNotification("正しい分を入力してください。")
+            Return
+        End If
+        If Not Integer.TryParse(txtSeconds.Text, seconds) OrElse seconds < 0 OrElse seconds >= 60 Then
+            SpeakNotification("正しい秒を入力してください。")
+            Return
+        End If
+
+        ' 秒単位で時間を計算
+        remainingTime = (hours * 3600) + (minutes * 60) + seconds
+        UpdateTimerLabel()
+    End Sub
+
+    ' タイマーの開始ボタン
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        If remainingTime > 0 Then
+            timerCountdown.Interval = 1000 ' 1秒間隔
+            timerCountdown.Start()
+        Else
+            SpeakNotification("時間を設定してください。")
+        End If
+    End Sub
+
+    ' タイマーの停止ボタン
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        timerCountdown.Stop()
+    End Sub
+
+    ' タイマーの動作
+    Private Sub timerCountdown_Tick(sender As Object, e As EventArgs) Handles timerCountdown.Tick
+        If remainingTime >= 1 Then
+            remainingTime -= 1
+            UpdateTimerLabel()
+        Else
+            SpeakNotification($"Lifeが増加しました。現在のライフは{Life.Value + 1}個です。")
+            If Life.Value = 4 Then
+                Life.Value = 5
+                SpeakNotification("スタミナがマックスまで回復しました。停止します。")
+                timerCountdown.Stop()
+            Else
+                ' 0秒になった場合、自動で4時間を追加
+                remainingTime += 4 * 3600 ' 4時間を秒に変換して追加
+                Life.Value = Life.Value + 1
+                UpdateTimerLabel()
+            End If
+        End If
+    End Sub
+
+    ' ラベルの更新
+    Private Sub UpdateTimerLabel()
+        Dim hours As Integer = remainingTime \ 3600
+        Dim minutes As Integer = (remainingTime Mod 3600) \ 60
+        Dim seconds As Integer = remainingTime Mod 60
+        lblTimer.Text = $"{hours:00}:{minutes:00}:{seconds:00}"
+        AddDate.Text = "次回ライフ追加日時" & vbCrLf & DateTime.Now.Add(New TimeSpan($"{hours:00}", $"{minutes:00}", $"{seconds:00}")) _
+            & vbCrLf & "ライフMAX日時" & vbCrLf & DateTime.Now.Add(New TimeSpan((4 - Life.Value) * 4 + hours, minutes, seconds))
+    End Sub
+
+    Private Sub errorcorrection_Click(sender As Object, e As EventArgs) Handles errorcorrection.Click
+        remainingTime -= 1
+    End Sub
+
+
+
+    Private Sub Life_KeyUp(sender As Object, e As KeyEventArgs) Handles Life.KeyUp
+        If Life.Value = 5 Then
+            timerCountdown.Stop()
+            remainingTime = 14400
+        ElseIf Life.Value >= 6 Then
+            Life.Value = 5
+        End If
+    End Sub
+
+    Private Sub Life_KeyDown(sender As Object, e As KeyEventArgs) Handles Life.KeyDown
+        If Life.Value = 4 Then
+            timerCountdown.Start()
+        End If
+    End Sub
+    ' 音声通知を生成して再生
+    Private Sub SpeakNotification(message As String)
+        Try
+            synthesizer.SpeakAsync(message)
+        Catch ex As Exception
+            MessageBox.Show("音声通知の生成中にエラーが発生しました。", "エラー")
+        End Try
+    End Sub
+
+
+    '------------------------------------------
+    'UI
+    '------------------------------------------
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Me.TopMost() = True
+        Else
+            Me.TopMost() = False
+        End If
+    End Sub
+End Class
